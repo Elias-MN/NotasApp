52916ec20923dbc816e3319a50dac186195614af
Elías <elias010493@gmail.com>
2023-01-31 11:56:13 +0100

    Fixed errors and better performance done

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ let idColumn;
 let idCard;
 let columns;
 let currentElement;
-
-inputColor.addEventListener("input", SetColor);
-addColumnButton.addEventListener("click", AddNewColumn);
+let color1;
 
 //TODO:
-// https://stackdiary.com/change-css-variable-value-javascript/
-// Bug: Cambiar el color sin haber creado nunca columnas
+// Añadir un segundo color para botones: https://stackdiary.com/change-css-variable-value-javascript/
+// Cambiar cursor a grab, grabbing, drop... cuando se arrastren tarjetas: https://www.w3schools.com/cssref/pr_class_cursor.php
 
 class column {
     constructor(id) {
@@ -22,6 +20,7 @@ class column {
     title = "";
     cards = [];
 };
+
 class card {
     constructor(id) {
         this.id = id;
@@ -29,82 +28,92 @@ class card {
     text = "";
 }
 
-function CheckLS() {
+// Función que recupera los datos de memoria
+function checkLS() {
     let tableJSON = localStorage.getItem('table');
+
+    // Sólo se ejecutará esta condición al inicio de la aplicación
     if (tableJSON == null) {
         idColumn = 0;
         idCard = 0;
+        color1 = "#7575ed";
         table.idColumnCounter = idColumn;
         table.idCardCounter = idCard;
+        table.backgroundColor = color1;
         columns = [];
     } else {
         table = JSON.parse(tableJSON);
         idColumn = table.idColumnCounter;
         idCard = table.idCardCounter;
-        columns = table.columns;
-        UpdateApp();
+        color1 = table.backgroundColor;
+        if (table.columns) {
+            columns = table.columns;
+        } else {
+            columns = [];
+        }
+        printColumns();
     }
+    container.style.background = color1;
+    inputColor.value = color1;
 }
 
-function UpdateApp() {
+// Función que actualiza las columnas en pantalla
+function printColumns() {
     columns.forEach(dataColumn => {
-        let divColumn = CreateColumn(dataColumn);
+        let divColumn = createColumn(dataColumn);
         container.insertBefore(divColumn, addColumnButton);
     });
 }
 
-function SetColor() {
-    table.backgroundColor = inputColor.value;
-    updateLSTable();
+// Función que actualiza los colores en pantalla
+function setColor() {
     container.style.background = inputColor.value;
 }
 
-function GetColor() {
-    let tableJSON = localStorage.getItem('table');
-    if (tableJSON == null) { return; }
-    table = JSON.parse(tableJSON);
-    if (table.backgroundColor == null) { return; }
-    inputColor.value = table.backgroundColor;
-    container.style.background = table.backgroundColor;
-}
-
-function CreateColumn(dataColumn) {
+// Función que crea la estructura de una columna
+function createColumn(dataColumn) {
     let divColumn = document.createElement("div");
     let title = "";
 
+    // Si la columna estaba en memoria le asigno sus valores, sino los pongo nuevos
     if (dataColumn) {
         divColumn.setAttribute("id", dataColumn.id);
         title = dataColumn.title;
     } else {
         divColumn.setAttribute("id", idColumn);
+        idColumn++;
+        table.idColumnCounter = idColumn;
     }
 
     divColumn.classList.add("column");
 
     divColumn.innerHTML = `
                                     <div>
-                                        <p class="deleteColumn" onclick="DeleteColumn(event)">Eliminar Columna</p>
+                                        <p class="deleteColumn" onclick="deleteColumn(event)">Eliminar Columna</p>
                                     </div>
                                     <div class="menuColumn">
-                                        <textarea onblur="TextTitleChanged(event)" class="title" placeholder="Insertar título" onkeydown="if(event.keyCode === 13) event.preventDefault();">${title}</textarea>
+                                        <textarea onblur="updateLSTable()" class="title" placeholder="Insertar título" onkeydown="if(event.keyCode === 13) event.preventDefault();">${title}</textarea>
                                     </div>
-                                    <div ondrop="CardDrop(event)" ondragover="AllowDrop(event)" class="drop-container">+</div>
-                                    <button class="addCard" onclick="AddNewCard(this)">Añadir tarjeta</button>
+                                    <div ondrop="dropCard(event)" ondragover="allowDrop(event)" class="drop-container">+</div>
+                                    <button class="addCard" onclick="addNewCard(this)">Añadir tarjeta</button>
                                     `;
 
+    // Si la columna estaba en memoria, le asigno sus tarjetas
     if (dataColumn) {
         for (let card = 0; card < dataColumn.cards.length; card++) {
-            AddNewCard(divColumn.children[0], dataColumn.cards[card]);
+            let columnReference = divColumn.children[0];
+            let currentCard = dataColumn.cards[card];
+            addNewCard(columnReference, currentCard);
         }
     }
 
-    idColumn++;
     return divColumn;
 }
 
-function AddNewColumn() {
+// Función que añade una columna a la tabla
+function addNewColumn() {
     let columnObj = new column(idColumn);
-    let divColumn = CreateColumn();
+    let divColumn = createColumn();
     container.insertBefore(divColumn, addColumnButton);
     table.idColumnCounter = idColumn;
     columns.push(columnObj);
@@ -112,48 +121,12 @@ function AddNewColumn() {
     updateLSTable();
 }
 
-function updateLSTable() {
-    let color1 = container.children[0].getElementsByClassName("inputColor")[0].value;
-    table.backgroundColor = color1;
-
-    let updateColumns = [];
-    let updateCards = [];
-    for (let element = 1; element < container.children.length - 1; element++) {
-        let currentColumn = container.children[element];
-        let currentColumnID = currentColumn.getAttribute('id');
-        let currentColumnTitle = currentColumn.getElementsByClassName("title")[0].value;
-        let currentColumnCards = currentColumn.querySelectorAll("[draggable]");
-
-        //console.log(currentColumnCards[0].firstChild.value);
-        let columnObj = new column(currentColumnID);
-        columnObj.title = currentColumnTitle;
-
-        for (let element = 0; element <= currentColumnCards.length; element++) {
-            let currentCard = currentColumnCards[element];
-            let currentCardValue = currentCard.value;
-            let currentCardID = currentCard.getAttribute('id');
-            let cardObj = new card(currentCardID);
-            cardObj.text = currentCardValue;
-            updateCards.push(cardObj);
-        }
-        columnObj.cards = updateCards;
-
-        updateColumns.push(columnObj);
-
-    }
-
-    console.log(updateColumns);
-    //table.columns = updateColumns;
-
-    let tableToJSON = JSON.stringify(table);
-    localStorage.setItem('table', tableToJSON);
-}
-
-function AddNewCard(button, dataCard) {
+// Función que crea la estructura de una tarjeta y la añade a la columna correspondiente
+function addNewCard(columnReference, dataCard) {
     let divCard = document.createElement("div");
     divCard.setAttribute("draggable", "true");
-    let cardObj = new card(idCard);
 
+    // Si la tarjeta estaba en memoria le asigno sus valores, sino los pongo nuevos
     if (dataCard) {
         divCard.setAttribute("id", dataCard.id);
     } else {
@@ -162,86 +135,95 @@ function AddNewCard(button, dataCard) {
         table.idCardCounter = idCard;
     }
 
-    let idCurrentColumn = button.parentNode.id;
-
-    if (!dataCard) {
-        table.columns.forEach((element, index) => {
-            if (element.id == idCurrentColumn) {
-                table.columns[index].cards.push(cardObj);
-                updateLSTable();
-            }
-        });
-    }
-
-    divCard.addEventListener('dragstart', CardDragStart);
+    divCard.addEventListener('dragstart', dragStartCard);
     let textarea = document.createElement("textarea");
+
+    // Si la tarjeta estaba en memoria le asigno su valor al textarea, sino se queda en blanco
     if (dataCard) {
         textarea.value = dataCard.text;
     }
-    textarea.setAttribute("onblur", "TextCardChanged(event)");
+    textarea.setAttribute("onblur", "updateLSTable()");
 
     textarea.classList.add("card");
     divCard.appendChild(textarea);
-    let dropContainer = button.parentNode.getElementsByClassName("drop-container");
-    button.parentNode.insertBefore(divCard, dropContainer[0]);
+    let dropContainer = columnReference.parentNode.getElementsByClassName("drop-container");
 
+    // Inserta la tarjeta por encima del elemento drop
+    columnReference.parentNode.insertBefore(divCard, dropContainer[0]);
+
+    // Si la tarjeta es nueva, se actualiza en memoria
+    if (!dataCard) {
+        updateLSTable();
+    }
 }
 
-function CardDrop(event) {
+// Función que suelta el elemento que se está arrastrando
+function dropCard(event) {
     let dropContainer = event.target.parentNode.getElementsByClassName("drop-container");
     event.target.parentNode.insertBefore(currentElement, dropContainer[0]);
+    updateLSTable();
 }
 
-function AllowDrop(event) {
+// Función que permite que a un elemento le puedan solar otros elementos
+function allowDrop(event) {
     event.preventDefault();
 }
 
-function CardDragStart(event) {
+// Función que actualiza el elemento que se está arrastrando
+function dragStartCard(event) {
     currentElement = event.target;
 }
 
-function CardDelete() {
+// Función que elimina una tarjeta
+function deleteCard() {
     currentElement.remove();
-
+    updateLSTable();
 }
 
-function DeleteColumn(event) {
-    let idDOM = event.target.parentNode.parentNode.id;
+// Función que elimina una columna
+function deleteColumn(event) {
     event.target.parentNode.parentNode.remove();
-    table.columns.forEach((element, index) => {
-        if (element.id == idDOM) {
-            table.columns.splice(index, 1);
-        }
-    });
-
     updateLSTable();
 }
 
-function TextTitleChanged(event) {
-    let idDOM = event.target.parentNode.parentNode.id;
-    table.columns.forEach((element, index) => {
-        if (element.id == idDOM) {
-            table.columns[index].title = event.target.value;
+// Función que actualiza todos los datos de la tabla en memoria (LocalStorage, Base de Datos...)
+function updateLSTable() {
+    table.backgroundColor = inputColor.value;
+
+    let updateColumns = [];
+    for (let element = 1; element < container.children.length - 1; element++) {
+        let updateCards = [];
+        let currentColumn = container.children[element];
+        let currentColumnID = currentColumn.getAttribute('id');
+        let currentColumnTitle = currentColumn.getElementsByClassName("title")[0].value;
+        let currentColumnCards = currentColumn.querySelectorAll("[draggable]");
+
+        let columnObj = new column(currentColumnID);
+        columnObj.title = currentColumnTitle;
+
+        for (let element = 0; element < currentColumnCards.length; element++) {
+            let currentCard = currentColumnCards[element];
+            let currentCardValue = currentCard.firstChild.value;
+            let currentCardID = currentCard.getAttribute('id');
+            let cardObj = new card(currentCardID);
+            cardObj.text = currentCardValue;
+            updateCards.push(cardObj);
         }
-    });
-    updateLSTable();
+        columnObj.cards = updateCards;
+        updateColumns.push(columnObj);
+    }
+
+    table.columns = updateColumns;
+    let tableToJSON = JSON.stringify(table);
+    localStorage.setItem('table', tableToJSON);
 }
 
-function TextCardChanged(event) {
-    let idColumnDOM = event.target.parentNode.parentNode.id;
-    let idCardDOM = event.target.parentNode.id;
-
-    table.columns.forEach((element, index) => {
-        if (element.id == idColumnDOM) {
-            table.columns[index].cards.forEach(card => {
-                if (card.id == idCardDOM) {
-                    card.text = event.target.value;
-                }
-            });
-        }
-    });
-    updateLSTable();
+// Función principal
+function main() {
+    inputColor.addEventListener("change", updateLSTable);
+    inputColor.addEventListener("input", setColor);
+    addColumnButton.addEventListener("click", addNewColumn);
+    checkLS();
 }
 
-CheckLS();
-GetColor();
+main();
